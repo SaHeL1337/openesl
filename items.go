37f7e4bb3a8d442b7ac196f751e20a950c1e16b4
SaHeL1337 <info@cloudvil.com>
2025-01-16 21:26:48 +0100

    change

--- items.go
+++ items.go
@@ -11,6 +11,7 @@ type Item struct {
 }
 
 func getSampleItems(amount int) *[]Item {
+	//sample comment
 	var items []Item
 	for i := 0; i < amount; i++ {
 		items = append(items, Item{id: i, name: "item", price: rand.Float64()})
